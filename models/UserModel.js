72b03461bf1afa4704d1f95ca59cdef4bf6dba3d
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-14 10:36:35 -0300

    Create UserModel.js

---
+++ models/UserModel.js
@@ -0,0 +1,18 @@
+// models/UserModel.js
+const API_URL = "https://6873e2e0c75558e273558c87.mockapi.io/projeto/user";
+
+export class UserModel {
+  static async getAll() {
+    const res = await fetch(API_URL);
+    return res.json();
+  }
+
+  static async create(data) {
+    const res = await fetch(API_URL, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    });
+    return res.json();
+  }
+}
