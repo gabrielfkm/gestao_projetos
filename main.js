54892a1a09911739dcca67b6a96d717edd544bb4
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-14 10:41:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,19 @@ if (currentPage.includes("/pages/login.html")) {
 } else if (currentPage.includes("/pages/agenda.html")) {
   EventController.init();
 }
+
+function verificarAutenticacao() {
+  const user = localStorage.getItem("usuarioLogado");
+  const paginaAtual = window.location.pathname;
+
+  const paginaPublica = paginaAtual.includes("/pages/login.html");
+
+  if (!user && !paginaPublica) {
+    window.location.href = "login.html";
+  }
+
+  if (user && paginaPublica) {
+    // já está logado e tentando voltar ao login
+    window.location.href = "projetos.html";
+  }
+}
