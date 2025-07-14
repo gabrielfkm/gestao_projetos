31ba1fa14180f7da09dad119c97261149d71a2ab
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-14 06:09:08 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+import { AuthController } from './controllers/AuthController.js';
+import { ProjectController } from './controllers/ProjectController.js';
+import { TaskController } from './controllers/TaskController.js';
+import { EventController } from './controllers/EventController.js';
+
+const currentPage = window.location.pathname;
+
+if (currentPage.includes("/pages/login.html")) {
+  AuthController.init();
+} else if (currentPage.includes("/pages/projetos.html")) {
+  ProjectController.init();
+} else if (currentPage.includes("/pages/tarefas.html")) {
+  TaskController.init();
+} else if (currentPage.includes("/pages/calendario.html")) {
+  EventController.init();
+}
